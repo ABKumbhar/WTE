784980b46e45cf9ff8b96c3a8a4ee075134a7bf6
Aniket Kumbhar <akumbhar@ch.iitr.ac.in>
2020-04-19 18:50:30 +0530

    Defined Variables

---
+++ WTE.m
@@ -0,0 +1,14 @@
+%% Waste to engregy(WTE)
+% Aniket Babasaheb Kumbhar(17112014)
+% G R Rakshith (17112030)
+% Deonar plant - Mumbai WTE model
+
+%% Defining variables
+% Subscripts :  
+%AD indicates anarobic digestion
+% T is non recylable waste
+% R is recyclable
+% MSW = municipal solid waste
+% OF organic fraction
+% Variable Q indiactes fraction
+
